f36c5fb0afa456a19438927bd4006cc564372997
MoistShinobi <85607599+MoistShinobi@users.noreply.github.com>
2023-04-21 19:01:50 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+let input = "Hello, how are you doing today?";
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+  if (input[i] === 'e') {
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++){
+    if (input[i] === vowels[j]){
+      resultArray.push(input[i]);
+      if (input[i] === 'u'){
+        resultArray.push(input[i]);
+      }
+    }
+  }
+};
+
+let resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
